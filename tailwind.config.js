74f4960c70412bc040dab6c040afc292ec41083d
Rishav <rishav.sharan@live.com>
2020-09-11 03:15:23 +0530

    Baisic layout done

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,6 @@
-const { colors } = require('tailwindcss/defaultTheme')
+const {
+    colors
+} = require('tailwindcss/defaultTheme')
 
 module.exports = {
     purge: [
@@ -7,12 +9,32 @@ module.exports = {
     theme: {
         extend: {
             colors: {
-                primary: colors.red[500],
-                secondary: colors.red[200],
-                neutral: colors.gray[200],
-                accented: colors.blue[400],
-                background: colors.gray[100],
-                control: colors.gray[200]
+                primary: {
+                    base: "#1B5790",
+                    light: "#4995DC",
+                    dark: "#12395E"
+                },
+                secondary: {
+                    base: "#1c927c",
+                    light: "#49DCC1",
+                    dark: "#125E50"
+                },
+                danger: {
+                    base: "#1B5790",
+                    light: "#FFFFFF",
+                    dark: "#CCCCCC"
+                },
+                background: {
+                    base: "#F6FDFC",
+                    light: "#FFFFFF",
+                    dark: "#CCCCCC"
+                },
+                fontcolor: {
+                    base: "#1B5790",
+                    light: "#FFFFFF",
+                    dark: "#CCCCCC"
+                },
+
             }
         },
     },
